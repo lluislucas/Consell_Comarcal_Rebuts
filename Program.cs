a604271800fdc0bd37b2fccc68cf90630835e5e3
Lluís Lucas <lluislucasfalguera@gmail.com>
2026-04-09 16:27:39 +0200

    rebut acabat

--- Program.cs
+++ Program.cs
@@ -26,80 +26,107 @@ if(cp=="")
 {
     cp="17472";
 }
-
+Console.WriteLine("digues el teu poble");
+string poble = Console.ReadLine();
+if(poble=="")
+{
+    cp="l'Armentera";
+}
+//busquem un poble per nom o CP
+//per cada persona del poble recorrem tots els habitatges
 
 //fem peticio a l API1
 var response = await client.GetFromJsonAsync<List<Habitatge>>("/Habitatges/" + dni);
 
+var response2 = await client.GetFromJsonAsync<List<Habitatge>>("/ContribuentsMateixPoble/" + poble);
+
 
 //fer bucle response que miri els cp, si cp igual sum a metros 
-
-int num_casesTot =0;
-int num_pisosTot =0;
-int num_terrenysTot =0;
-
-int m2casesTot =0;
-int m2pisosTot =0;
-int m2terrenysTot =0;
 
 double descomptefamNumerosa = 0.9;
 double recarrecMenors = 1.05;
 
-double quotaCases = 0;
-double quotaPisos = 0;
-double quotaTerrenys = 0;
-
-
-foreach (var h in response)
+//foreacho titular en titulars habitatges
+foreach(var titular in response2)
 {
+    int num_casesTot =0;
+    int num_pisosTot =0;
+    int num_terrenysTot =0;
     bool descomptetassa = false;
-   if ( cp == h.CodiPostal )
+
+    int m2casesTot =0;
+    int m2pisosTot =0;
+    int m2terrenysTot =0;
+
+    double quotatotal = 0;
+
+    foreach (var h in response)
     {
-        switch (h.TipusImmoble)
-        {
-            case "Casa":
-            num_casesTot ++;
-            m2casesTot += h.MetresQuadrats;
-            double preuM2Casa = 0.998;
-            if(h.EsFamiliaNumerosa == true)
-                {
-                    preuM2Casa = preuM2Casa*descomptefamNumerosa;
-                     descomptetassa = true;
-                }
-            if(h.NumMenorsImmoble>0)
-                {
-                    preuM2Casa = preuM2Casa*recarrecMenors;
-                }
-             quotaCases = h.MetresQuadrats*preuM2Casa;
-            break;
-
-            case "Pis":
-            num_pisosTot ++;
-            m2pisosTot += h.MetresQuadrats;
-            double preuM2Pis = 0.996;
-             if(h.EsFamiliaNumerosa == true)
-                {
-                    preuM2Pis = preuM2Pis*descomptefamNumerosa;
-                     descomptetassa = true;
-                }
-            quotaPisos = h.MetresQuadrats*preuM2Pis;
-            break;
-
-            case "Terreny":
-            num_terrenysTot ++;
-            m2terrenysTot += h.MetresQuadrats;
-            double preuM2terreny = 0.136;
-            quotaTerrenys = h.MetresQuadrats*preuM2terreny;
-            break;
-
-        }
-
         
-    } 
+    if ( cp == h.CodiPostal )
+        {
+            switch (h.TipusImmoble)
+            {
+                case "Casa":
+                num_casesTot ++;
+                m2casesTot += h.MetresQuadrats;
+                double preuM2Casa = 0.998;
+                if(h.EsFamiliaNumerosa == true)
+                    {
+                        preuM2Casa = preuM2Casa*descomptefamNumerosa;
+                        descomptetassa = true;
+                    }
+                if(h.NumMenorsImmoble>0)
+                    {
+                        preuM2Casa = preuM2Casa*recarrecMenors;
+                    }
+                quotatotal = quotatotal + h.MetresQuadrats*preuM2Casa;
+                break;
 
+                case "Pis":
+                num_pisosTot ++;
+                m2pisosTot += h.MetresQuadrats;
+                double preuM2Pis = 0.996;
+                if(h.EsFamiliaNumerosa == true)
+                    {
+                        preuM2Pis = preuM2Pis*descomptefamNumerosa;
+                        descomptetassa = true;
+                    }
+                quotatotal = quotatotal + h.MetresQuadrats*preuM2Pis;
+                break;
+
+                case "Terreny":
+                num_terrenysTot ++;
+                m2terrenysTot += h.MetresQuadrats;
+                double preuM2terreny = 0.136;
+                quotatotal = quotatotal + h.MetresQuadrats*preuM2terreny;
+                break;
+
+            }   
+        } 
+    }
+Console.WriteLine("............................");
+
+Console.WriteLine("CONSELL COMARCAL - Taxa Especial sobre els Edificis Comarcals (TEEC)");
+
+Console.WriteLine($"Nom:{titular.NomContribuent} " );
+Console.WriteLine($"Població:{titular.Poblacio} " );
+Console.WriteLine($"Cases: {num_casesTot} - {m2casesTot}" );
+Console.WriteLine($"Pisos: {num_pisosTot} - {m2pisosTot}" );
+Console.WriteLine($"Terrenys: {num_terrenysTot} - {m2terrenysTot}" );
+
+if (descomptetassa == true)
+    {
+        Console.WriteLine($"S'ha aplicat descompte per familia numerosa del 10%");
+    }
+
+Console.WriteLine($"Quota: {quotatotal}€" );
+
+Console.WriteLine("............................");
 }
 
-//Calcul quota a pagar
+
+
 
 
 
